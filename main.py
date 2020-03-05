2536340c56f0513cd7facc8f704635d1b7e46684
jb-0001 <jonas.baars@hotmail.com>
2020-03-05 21:20:56 +0100

    Code style correction, removed pytest.ini, updated .gitignore

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 
-file = input("Please enter the filename and extension you want to analyze.\nNote: The file needs to be in the same directory as the program.\n ")
+file = input("""Please enter the filename and extension you want to analyze.\n
+Note: The file needs to be in the same directory as the program.\n """)
 video = cv2.VideoCapture(file)
 
 ret, frame1 = video.read()
@@ -11,9 +12,9 @@ ret, frame2 = video.read()
 while video.isOpened():
     difference = cv2.absdiff(frame1, frame2)
     grayscale = cv2.cvtColor(difference, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(grayscale, (5,5), 0)
+    blur = cv2.GaussianBlur(grayscale, (5, 5), 0)
     _, threshold = cv2.threshold(blur, 35, 255, cv2.THRESH_BINARY)
-    dilated = cv2.dilate(threshold, None, iterations = 2)
+    dilated = cv2.dilate(threshold, None, iterations=2)
     contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
@@ -22,12 +23,12 @@ while video.isOpened():
             continue
         cv2.drawContours(frame1, contours, -1, (0, 255, 0), 1)
         cv2.rectangle(frame1, (x, y), (x+w, y+h), (120, 0, 150), 2)
-        
+
     cv2.imshow("feed", frame1)
     frame1 = frame2
     ret, frame2 = video.read()
 
-    if cv2.waitKey(40) == 27:
+    if cv2.waitKey(50) == 27:
         break
 
 cv2.destroyAllWindows()
